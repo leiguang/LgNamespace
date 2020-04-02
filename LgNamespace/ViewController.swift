79afc8ca641f558814d1b21a3fcbb95bbfe3be7d
Guang Lei <575403441@qq.com>
2020-04-02 11:54:37 +0800

    Initial Commit

---
+++ LgNamespace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LgNamespace
+//
+//  Created by Guang Lei on 2020/4/2.
+//  Copyright © 2020 Guang Lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
